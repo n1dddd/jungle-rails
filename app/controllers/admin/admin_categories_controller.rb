5f71c2acddb54fb86d4d4ce77f6f7c24774e2024
Daniel <danielseniv@outlook.com>
2023-05-23 19:05:45 -0400

    Initialize controller with index, and add view to display categories

--- app/controllers/admin/admin_categories_controller.rb
+++ app/controllers/admin/admin_categories_controller.rb
@@ -1,5 +1,9 @@
 class Admin::AdminCategoriesController < ApplicationController
+    http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"]
+    
     def index
+        @categories = Category.order(id: :desc).all
     end
 
     def new
@@ -7,5 +11,5 @@ class Admin::AdminCategoriesController < ApplicationController
 
     def create
     end
-    
+
 end
