0ce0763c501fb5308e180b54c0bd6199674e0fdf
Daniel <danielseniv@outlook.com>
2023-05-28 15:14:23 -0400

    Add validation tests for error messages for empty price, quantity, and category arguments for @product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Product, type: :model do
     @category.save
 end
 
-  describe 'Validation' do
+  describe 'validation' do
     it "should save a product with all arguments satisfied" do
       @product = Product.new(name: 'Seqouia', price: 1500, quantity: 12, category: @category)
       expect(@product).to be_valid
@@ -16,6 +16,24 @@ end
       @product = Product.new(name: nil, price: 1200, quantity: 10, category: @category)
       @product.save
       expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "should not be valid if the price argument is missing/nil" do
+      @product = Product.new(name: 'Seqouia', quantity: 10, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "should not be valid if the quantity argument is missing/nil" do
+      @product = Product.new(name: 'Seqouia', price: 10000, quantity: nil, category: @category)
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "should not be valid if the category argument is missing/nil" do
+      @product = Product.new(name: 'Seqouia', price: 10000, quantity: 20, category: nil)
+      @product.save
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 
