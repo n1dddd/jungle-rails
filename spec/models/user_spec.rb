f8e7ae1b5fb621db1e0d9c5b55180e7e86c5dcbc
Daniel <danielseniv@outlook.com>
2023-05-28 18:43:41 -0400

    Add test for if email is not set, email is not unique, login and case-insensitive login

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,5 +26,32 @@ RSpec.describe User, type: :model do
       @user.save
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
+    
+    it "should have an erorr if email is not set" do
+      @user = User.new(name: 'Daniel', email: nil, password: '123HELLO', password_confirmation: '123HELLO' )
+      @user.save
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "should have throw an error if email is not unique" do
+      @user_1 = User.new(name: 'Daniel', email: 'ds1@gmail.com', password: '123HELLO', password_confirmation: '123HELLO' )
+      @user_1.save
+      @user_2 = User.new(name: 'Lana', email: 'ds1@gmail.com', password: '123HELLO', password_confirmation: '123HELLO')
+      @user_2.save
+      expect(@user_2.errors.full_messages).to include("Email has already been taken")
+    end
+  end
+  describe '.authenticate_with_credentials' do
+    it 'logs in user with correct credentials' do
+      @user_1 = User.new(name: 'Daniel', email: 'ds1@gmail.com', password: '123HELLO', password_confirmation: '123HELLO' )
+      @user_1.save
+      @user_logged_in = User.http_basic_authenticate_with('ds1@gmail.com', '123HELLO')
+      expect(@user_logged_in).to_not eq(nil)
+    end
+    it 'should log in user if email is in wrong case' do
+      @user_1 = User.new(name: 'Daniel', email: 'DS1@gmail.com', password: '123HELLO', password_confirmation: '123HELLO' )
+      @user_1.save
+      @user_logged_in = User.http_basic_authenticate_with('ds1@gmail.com', '123HELLO')
+      expect(@user_logged_in).to_not eq(nil)
+    end
   end
 end
