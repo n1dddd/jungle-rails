03e632441497f61d5f42a601b2cc5476102a0d63
Daniel <danielseniv@outlook.com>
2023-05-31 00:08:30 -0400

    Add second failing test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,4 +3,7 @@ describe('Visits homepage', () => {
     it('test if cypress connects to the homepage', () => {
         cy.visit('http://localhost:3000')
     })
+    it('test if there is a product on the homepage', () => {
+        cy.get(".products article").should("be.visible")
+    });
 });
